c5e16322aa16e8c26cecc5a513e239b8ec441b61
Filip Pieronkiewicz <987654321fp@gmail.com>
2019-02-08 16:39:45 +0100

    Commit

--- Server/settings.py
+++ Server/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'chat',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,7 +51,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = (
+   'null',
+    'http://localhost:1234/czat',
+    'http://127.0.0.1:8080',
+)
 ROOT_URLCONF = 'Server.urls'
 
 TEMPLATES = [
--- chat/views.py
+++ chat/views.py
@@ -2,6 +2,9 @@ from typing import Any, Union, List
 from django.db.models import QuerySet
 from django.http import JsonResponse
 from datetime import timedelta
+
+from django.views.decorators.http import require_GET
+
 from .models import Mes
 from django.utils import timezone
 
@@ -23,4 +26,9 @@ def handler(request):
     if lastid is not None:
         filtered_data: QuerySet = Mes.objects.filter(id__gte=lastid).values()
         response_data: List = list(filtered_data.values())
-        return JsonResponse(response_data, safe=False)
+        response = JsonResponse(response_data, safe=False)
+        response["Access-Control-Allow-Origin"] = "*"
+        response["Access-Control-Allow-Methods"] = "GET, OPTIONS, POST, PUT, DELETE, "
+        response["Access-Control-Max-Age"] = "1000"
+        response["Access-Control-Allow-Headers"] = "X-Requested-With, Content-Type"
+        return response
